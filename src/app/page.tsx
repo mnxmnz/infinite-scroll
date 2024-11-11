07c115012f91c87dc29f194ef9741c2005abf308
mnxmnz <minzidev@gmail.com>
2024-11-12 00:30:28 +0900

    refactor: replace InfiniteScrollExample with InfiniteScrollList in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,9 @@
-'use client';
+import InfiniteScrollList from '@/components/InfiniteScrollList';
 
-import Image from 'next/image';
-
-import useInfiniteScroll from '@/hooks/useInfiniteScroll';
-
-interface Item {
-  id: number;
-  title: string;
-  imageUrl: string;
-}
-
-const InfiniteScrollExample = () => {
-  const getItems = async (offset: number, limit: number): Promise<Item[]> => {
-    const response = await fetch(`/api/items?offset=${offset}&limit=${limit}`);
-    const data = await response.json();
-
-    return data;
-  };
-
-  const { data, isFetchingNextPage, targetRef } = useInfiniteScroll<Item>({
-    queryKey: ['/items'],
-    queryFn: getItems,
-  });
-
+export default function Home() {
   return (
-    <ul>
-      {data?.map(item => (
-        <li key={item.id}>
-          <div>{item.title}</div>
-          <Image src={item.imageUrl} alt={item.title} width={100} height={100} />
-        </li>
-      ))}
-      <li ref={targetRef}>{isFetchingNextPage && <div>Loading...</div>}</li>
-    </ul>
+    <main>
+      <InfiniteScrollList />
+    </main>
   );
-};
-
-export default InfiniteScrollExample;
+}
