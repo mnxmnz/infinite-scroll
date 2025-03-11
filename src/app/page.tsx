e188e0bdaab6e4dbe301d9b0642ca67f39729889
mnxmnz <minzidev@gmail.com>
2025-03-12 00:39:36 +0900

    feat: replace InfiniteScrollList with AutoLoadMoreList and ManualLoadMoreList components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import InfiniteScrollList from '@/components/InfiniteScrollList';
+import AutoLoadMoreList from '@/components/AutoLoadMoreList';
+import ManualLoadMoreList from '@/components/ManualLoadMoreList';
 
 export default function Home() {
   return (
-    <main>
-      <InfiniteScrollList />
+    <main style={{ display: 'flex' }}>
+      <div style={{ width: '50%' }}>
+        <AutoLoadMoreList />
+      </div>
+      <div style={{ width: '50%' }}>
+        <ManualLoadMoreList />
+      </div>
     </main>
   );
 }
