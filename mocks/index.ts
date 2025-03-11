90dc17557aa40d6de5a5051072e092d6321ccb36
mnxmnz <minzidev@gmail.com>
2025-03-12 00:46:39 +0900

    feat: add conditional mock initialization based on environment variable

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,4 +1,8 @@
 export default async function initMocks() {
+  if (process.env.NEXT_PUBLIC_USE_MOCK !== 'true') {
+    return;
+  }
+
   if (typeof window === 'undefined') {
     const { server } = await import('./server');
     server.listen({ onUnhandledRequest: 'bypass' });
